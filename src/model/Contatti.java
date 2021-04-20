4a6b0fcfd4162aaa91ce410d279b4bcd01372b17
miele.riccardo <miele.riccardo@TAVE-PC28.OSDBSESTO.local>
2021-04-20 10:53:12 +0200

    rub

---
+++ src/model/Contatti.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+
+
+/**
+ *
+ * @author miele.riccardo
+ */
+public class Contatti {
+    private String id,nome,cognome, via,citta;
+
+    public Contatti(String id, String nome, String cognome, String via, String citta) {
+        this.id = id;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.via = via;
+        this.citta = citta;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getVia() {
+        return via;
+    }
+
+    public void setVia(String via) {
+        this.via = via;
+    }
+
+    public String getCitta() {
+        return citta;
+    }
+
+    public void setCitta(String citta) {
+        this.citta = citta;
+    }
+
+   
+}
